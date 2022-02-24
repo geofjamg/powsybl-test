d7896b9390e65aa672c9c6f8af8db2f7ab1ac524
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-02-24 10:47:15 +0100

    Add diagram
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/com/powsybl/test/Test.java
+++ src/main/java/com/powsybl/test/Test.java
@@ -1,12 +1,17 @@
 package com.powsybl.test;
 
-import com.powsybl.iidm.network.Network;
 import com.powsybl.ieeecdf.converter.IeeeCdfNetworkFactory;
+import com.powsybl.iidm.network.Network;
 import com.powsybl.loadflow.LoadFlow;
+import com.powsybl.nad.NetworkAreaDiagram;
+
+import java.nio.file.Paths;
 
 public class Test {
     public static void main(String[] args) {
         Network network = IeeeCdfNetworkFactory.create14();
         LoadFlow.run(network);
+        new NetworkAreaDiagram(network)
+                .draw(Paths.get("test.svg"));
     }
 }
