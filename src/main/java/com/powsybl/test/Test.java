bb06e97fc61a58bbd98a649d475fed9f104a204f
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-06-13 20:29:00 +0200

    New test

--- src/main/java/com/powsybl/test/Test.java
+++ src/main/java/com/powsybl/test/Test.java
@@ -1,17 +1,10 @@
 package com.powsybl.test;
 
-import com.powsybl.ieeecdf.converter.IeeeCdfNetworkFactory;
+import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.Network;
-import com.powsybl.loadflow.LoadFlow;
-import com.powsybl.nad.NetworkAreaDiagram;
-
-import java.nio.file.Paths;
 
 public class Test {
     public static void main(String[] args) {
-        Network network = IeeeCdfNetworkFactory.create14();
-        LoadFlow.run(network);
-        new NetworkAreaDiagram(network)
-                .draw(Paths.get("test.svg"));
+        Network network = Importers.loadNetwork("<FOLDER>/CGMES_v2_4_15_MicroGridTestConfiguration_BC_BE_v2.zip");
     }
 }
